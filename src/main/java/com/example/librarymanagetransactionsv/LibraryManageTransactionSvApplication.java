b84f965eaee1e2b8a150cb7353f8d65747cab278
Vinul Vinsura <vinulviksura16@gmail.com>
2024-06-02 00:56:59 +0530

    first commit

---
+++ src/main/java/com/example/librarymanagetransactionsv/LibraryManageTransactionSvApplication.java
@@ -0,0 +1,13 @@
+package com.example.librarymanagetransactionsv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryManageTransactionSvApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibraryManageTransactionSvApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/librarymanagetransactionsv/LibraryManageTransactionSvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.librarymanagetransactionsv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryManageTransactionSvApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
